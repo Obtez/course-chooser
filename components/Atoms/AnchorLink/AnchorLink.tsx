a3fc238f227450a9e8f846c724bc056e9604d429
Obtez <59846143+Obtez@users.noreply.github.com>
2022-02-05 23:35:56 +0100

    basic homepage AnchorLink now has variants =primary =secondary

--- components/Atoms/AnchorLink/AnchorLink.tsx
+++ components/Atoms/AnchorLink/AnchorLink.tsx
@@ -5,14 +5,15 @@ import styles from './AnchorLink.module.scss';
 type Props = {
   href: string;
   children: React.ReactNode;
+  variant?: 'primary' | 'secondary';
 }
 
-export default function AnchorLink({ href, children }: Props) {
+export default function AnchorLink({ href, children, variant }: Props) {
   if (!href || !children) return null;
 
   return (
     <Link href={href}>
-      <a className={`${styles.btn}`}>{ children }</a>
+      <a className={`${styles.btn} ${styles[variant || '']}`}>{ children }</a>
     </Link>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import styles from '../styles/Home.module.scss';
+import AnchorLink from "../components/Atoms/AnchorLink/AnchorLink";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
+  return (<div className={styles.page__container}>
+    <h1 className={styles.page__title}>Welcome</h1>
+      <p className={styles.page__slogan}>Login to choose the courses you are interested in or manage the ones
+        you have already applied to.</p>
+
+    <div className={styles.btn__container}>
+      <AnchorLink href={'/courses'} variant={'primary'}>All Courses</AnchorLink>
+      <AnchorLink href={'/profile'} variant={'secondary'}>Your Courses</AnchorLink>
     </div>
-  )
+    </div>)
 }
