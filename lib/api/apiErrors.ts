5f97a16d02f8a38b5517f3617136a0802534e776
Obtez <59846143+Obtez@users.noreply.github.com>
2022-02-08 21:56:07 +0100

    implemented updateFailedError helper

--- lib/api/apiErrors.ts
+++ lib/api/apiErrors.ts
@@ -11,5 +11,9 @@ export function notProvidedError(res: NextApiResponse, content: any, contentType
 }
 
 export function wrongRequestMethodError(res: NextApiResponse) {
-  return res.status(400).json({ message: `Wrong request method.` });
+  return res.status(405).json({ message: `Request method not allowed.` });
+}
+
+export function updateFailedError(res: NextApiResponse, execType: string, reason: string) {
+  return res.status(500).json({ message: `${execType} failed: ${reason}.` });
 }
