8ea19a91145f1012b480031f71dd34f233dafd48
Obtez <59846143+Obtez@users.noreply.github.com>
2022-02-08 21:28:43 +0100

    added wrongRequestMethodError helper

--- lib/api/apiErrors.ts
+++ lib/api/apiErrors.ts
@@ -3,3 +3,13 @@ import {NextApiResponse} from "next";
 export function notFoundError(res: NextApiResponse, message: string) {
   return res.status(404).json({ message });
 }
+
+export function notProvidedError(res: NextApiResponse, content: any, contentType: string) {
+    if (!content) {
+      return res.status(406).json({ message: `No ${contentType} provided.` })
+    }
+}
+
+export function wrongRequestMethodError(res: NextApiResponse) {
+  return res.status(400).json({ message: `Wrong request method.` });
+}
