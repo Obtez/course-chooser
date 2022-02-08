9c12d2173c8e5ca4d0c6f2d539ca79637ce85846
Obtez <59846143+Obtez@users.noreply.github.com>
2022-02-08 23:04:49 +0100

    created api/student/courses/update/[id] route created api/user/role/[id] route

---
+++ pages/api/user/role/[id].ts
@@ -0,0 +1,26 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {
+  checkIfProvided, notFoundError,
+  wrongRequestMethodError
+} from "../../../../lib/api/apiErrors";
+import {getUserRole} from "../../../../lib/api/userHelper";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const id = req.query.id.toString();
+
+  checkIfProvided(res, id, `user ID`);
+
+  if (req.method === 'GET') {
+    const role = await getUserRole(id);
+
+    if (!role) {
+      notFoundError(res, 'Error finding user role in database.');
+    } else {
+      res.status(200).json({ role: role });
+    }
+  }
+
+
+  // If request is not supported
+  wrongRequestMethodError(res);
+}
